cb27281a121f4baf100bcc3aee1ad3a63892476c
Steve Samuel <92751312+CodeSteve1@users.noreply.github.com>
2025-01-25 00:42:36 +0530

    Add files via upload

---
+++ HOST_COMMUNICATION.ino
@@ -0,0 +1,131 @@
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <Wire.h>
+
+
+
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress[] = {0x40, 0x91, 0x51, 0xfd, 0xbe, 0x0c};
+
+// Define variables to store BME280 readings to be sent
+float temperature;
+float humidity;
+float pressure;
+
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+float incomingPres;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float hum;
+    float pres;
+} struct_message;
+
+// Create a struct_message called data to hold sensor readings
+struct_message data;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingTemp = incomingReadings.temp;
+  incomingHum = incomingReadings.hum;
+  incomingPres = incomingReadings.pres;
+  Serial.println("Hello from the HOST");
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  delay(2000);
+    Serial.println(WiFi.macAddress());
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+ 
+void loop() {
+  getReadings();
+ 
+  // Set values to send
+  data.temp = temperature;
+  data.hum = humidity;
+  data.pres = pressure;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &data, sizeof(data));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+delay(1000);
+}
+void getReadings(){
+  temperature = 420.34;
+  humidity = 823.3;
+  pressure = 4344.3;
+}
+
+void updateDisplay(){
+  // Display Readings on OLED Display
+  
+  // Display Readings in Serial Monitor
+  Serial.println("Hello from the Host");
+
+}
